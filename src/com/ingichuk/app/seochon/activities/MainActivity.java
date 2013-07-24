88ac8844c9c39cb3cae37eb75c0bdeeabcab11a7
Minsoo Park <minsoo1003@gmail.com>
2013-07-24 23:50:39 +0900

    메인 액티비티 메뉴 적용

--- src/com/ingichuk/app/seochon/activities/MainActivity.java
+++ src/com/ingichuk/app/seochon/activities/MainActivity.java
@@ -1,11 +1,57 @@
 package com.ingichuk.app.seochon.activities;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import com.ingichuk.app.seochon.R;
 
 
 public class MainActivity extends Activity {
+
+    private ImageView buttonMenuEat;
+    private ImageView buttonMenuDrink;
+    private ImageView buttonMenuPlay;
+    private ImageView buttonMenuRest;
+    private ImageView buttonMenuView;
+
+    private View.OnClickListener menuClickListener;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonMenuEat = (ImageView) findViewById(R.id.menu_eat);
+        buttonMenuDrink = (ImageView) findViewById(R.id.menu_drink);
+        buttonMenuPlay = (ImageView) findViewById(R.id.menu_play);
+        buttonMenuRest = (ImageView) findViewById(R.id.menu_rest);
+        buttonMenuView = (ImageView) findViewById(R.id.menu_view);
+
+
+        buttonMenuEat.setOnClickListener(menuClickListener);
+        buttonMenuDrink.setOnClickListener(menuClickListener);
+        buttonMenuPlay.setOnClickListener(menuClickListener);
+        buttonMenuRest.setOnClickListener(menuClickListener);
+        buttonMenuView.setOnClickListener(menuClickListener);
+
+        menuClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int tag = Integer.parseInt((String)v.getTag());
+                switch (tag) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                }
+            }
+        };
     }
 }
