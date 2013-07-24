9b068d0fdbc48e68e28688d454e7999472c73f32
Minsoo Park <minsoo1003@gmail.com>
2013-07-24 23:10:20 +0900

    스플래시 2.5초 후 메인 액티비티로

---
+++ src/com/ingichuk/app/seochon/activities/MainActivity.java
@@ -0,0 +1,11 @@
+package com.ingichuk.app.seochon.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- src/com/ingichuk/app/seochon/activities/SplashActivity.java
+++ src/com/ingichuk/app/seochon/activities/SplashActivity.java
@@ -1,16 +1,33 @@
 package com.ingichuk.app.seochon.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import com.ingichuk.app.seochon.R;
 
 public class SplashActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        handler.postDelayed(runnable, 2500);
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
