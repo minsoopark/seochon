39001fc05b858c89e71ee779661b2d33546433d4
박민수 <minsoo1003@gmail.com>
2013-07-25 10:38:49 +0900

    푸시링크에서 디바이스 체크하도록 수정

--- src/com/ingichuk/app/seochon/SeochonApplication.java
+++ src/com/ingichuk/app/seochon/SeochonApplication.java
@@ -1,6 +1,7 @@
 package com.ingichuk.app.seochon;
 
 import android.app.Application;
+import android.os.Build;
 import android.provider.Settings;
 import com.ingichuk.app.seochon.R;
 import com.pushlink.android.PushLink;
@@ -9,6 +10,7 @@ public class SeochonApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        PushLink.addMetadata("Phone", Build.DEVICE);
         PushLink.start(this, R.drawable.ic_launcher,
                 "2165mbuqr499kk9o",
                 Settings.Secure.getString(this.getContentResolver(),
